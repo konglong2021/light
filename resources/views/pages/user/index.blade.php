ec0430645a454a9c36ccfa8194af3954a1e00841
Khornlang SAM <khornlang.sam@sompom.com>
2021-08-09 21:24:57 +0700

    commit

---
+++ resources/views/pages/user/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.master')
+
+@push('plugin-styles')
+  <link href="{{ asset('assets/plugins/prismjs/prism.css') }}" rel="stylesheet" />
+@endpush
+
+@section('content')
+<div class="row">
+    Hello This is User list Page;
+</div>
+@endsection
+
+@push('plugin-scripts')
+  <script src="{{ asset('assets/plugins/prismjs/prism.js') }}"></script>
+  <script src="{{ asset('assets/plugins/clipboard/clipboard.min.js') }}"></script>
+@endpush
